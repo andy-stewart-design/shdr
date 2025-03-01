99d7840e6c19f1055860717742385715859b68f8
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-03-01 10:47:03 -0500

    removing vite-only demo implementation

--- demo/src/routes/basic/script.ts
+++
@@ -1,9 +0,0 @@
-import GlslRenderer from "@/../../dist";
-import frag from "./sketch.frag?raw";
-import "@/styles/reset.css";
-import "@/styles/demo.css";
-
-const container = document.querySelector("#app") as HTMLElement;
-
-const gl = new GlslRenderer(container, frag);
-gl.play();
