eda2733c4db7f4188cc3cd1ed02292d7ff27261b
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-04-02 08:13:15 -0400

    moving lil-gui to demo package json

--- demos/astro.config.mjs
+++ demos/astro.config.mjs
@@ -2,14 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    build: {
-      rollupOptions: {
-        // Either explicitly externalize it
-        external: ["lil-gui"],
-        // OR make sure it's properly resolved
-      },
-    },
-  },
-});
+export default defineConfig({});
