e4766fa1e51e4f6ea4d96679bfeb9c9717cf9691
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-04-02 07:45:38 -0400

    Trying to fix build issue

--- demos/astro.config.mjs
+++ demos/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        // Either explicitly externalize it
+        external: ["lil-gui"],
+        // OR make sure it's properly resolved
+      },
+    },
+  },
+});
